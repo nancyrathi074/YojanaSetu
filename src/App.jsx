bcc3a0f51c96c582a59a1020a6543c546c26bea1
nancyrathi074 <nancyrathi074@gmail.com>
2026-07-10 16:51:31 +0530

    Added footer and updated UI theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
 import Home from "./pages/Home";
@@ -12,39 +12,18 @@ import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-
+      <BrowserRouter>
       <Navbar />
 
       <Routes>
-
-        <Route
-          path="/"
-          element={<Home />}
-        />
-
-        <Route
-          path="/schemes"
-          element={<FindSchemes />}
-        />
-
-        <Route
-          path="/dashboard"
-          element={<Dashboard />}
-        />
-
-        <Route
-          path="/details"
-          element={<SchemeDetails />}
-        />
-
-        <Route
-          path="*"
-          element={<NotFound />}
-        />
-
+        <Route path="/" element={<Home />} />
+        <Route path="/schemes" element={<FindSchemes />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/details" element={<SchemeDetails />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
 
+      <Footer />
     </BrowserRouter>
   );
 }
